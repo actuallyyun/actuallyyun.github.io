fdf71960247144c4faf8e730b92d9463a52c3a3e
Yun Ji <this.jiyun@gmail.com>
2023-07-07 12:18:06 +0300

    add content to intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,34 +8,38 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            className='button button--secondary button--lg'
+            to='/docs/intro'
+          >
+            Who Am I ?
           </Link>
         </div>
       </div>
     </header>
-  );
+  )
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description='Explore a wealth of coding tips, 
+      job search strategies, and tech interview tips that I learned along the way.
+      Stay ahead in your software engineering career with expert insights, best practices, and practical resources on programming languages, frameworks, resume building, networking, and acing technical interviews. '
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
     </Layout>
-  );
+  )
 }
