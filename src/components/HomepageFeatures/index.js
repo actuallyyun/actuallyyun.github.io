de7b3cc0eefb308ca8256f05b1d64357d27a93a9
Yun Ji <this.jiyun@gmail.com>
2023-07-07 13:33:10 +0300

    update intro

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -13,7 +13,12 @@ const FeatureList = [
     description: (
       <>
         I started my carrer as a new reporter, then followed my curiosity to the
-        booming tech industry in Beijing, I transitioned to be a tech reporter.{' '}
+        booming tech industry in Beijing, I transitioned to be a tech reporter.
+        I covered breaking news in the tech industry, including Tesla's
+        expansion in China and developments in Uber; reported on emerging
+        startups that later achieved global success, such as TikTok. This
+        experience gave me a deep understanding of the tech industry and shaped
+        me into a storyteller through writing.
       </>
     )
   },
@@ -34,10 +39,11 @@ const FeatureList = [
     img: yunProgramming,
     description: (
       <>
-        How I wish I had studied computer science in college.However, learning
+        How I wish I had studied computer science in college! However, learning
         how much I love coding in my 30s is not too late. There's no such thing
         as too late. When we gain something, we always loose something. I am
-        glad I found my passion in coding.
+        grateful for the experiences I had, and I am excited for the new coding
+        adventure!
       </>
     )
   }
